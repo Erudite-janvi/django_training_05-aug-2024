a91b432409417954b0157be2286a4624eadadbba
Erudite-janvi <janvisharma@Janvis-MacBook-Pro.local>
2024-08-06 13:18:53 -0400

    Creating_models

--- login/admin.py
+++ login/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import User
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+     list_display = ('name','last_name','email')
+
+     search_fields = ('name','last_name','email')
+
+admin.site.register(User,UserAdmin)
--- login/models.py
+++ login/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=45)
+
+    def __str__(self):
+        return self.name + ' '+ self.last_name
+        
